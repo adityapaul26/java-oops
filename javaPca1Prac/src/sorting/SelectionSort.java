72a21030e49f8643744eacdebfab97d5d512b986
adityapaul26 <adityapaul2603@gmail.com>
2025-09-20 13:20:06 +0530

    gitignore add

--- javaPca1Prac/src/sorting/SelectionSort.java
+++ javaPca1Prac/src/sorting/SelectionSort.java
@@ -7,7 +7,7 @@ public class SelectionSort {
         for(int i=0;i<n-1;i++){
             int minIndex= i;
 
-            for(int j=i+1;j<n-1;j++){
+            for(int j=i+1;j<n;j++){
                 if(arr[j]<arr[minIndex])
                     minIndex= j;
             }
